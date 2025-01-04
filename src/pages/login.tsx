2cc2dfcfba5eeec2215dd2da5b2e758535ee1e99
GuihCFerreira <guihhh231@gmail.com>
2025-01-04 02:18:52 -0300

    criado a tela de login do usuario

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,10 @@
+import { useNavigate } from "react-router-dom";
 import LoginForm from "../components/login-form/login-form";
 import "../styles/login.css";
 
 const Login = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="container">
       <h1 className="titulo">Faça Login</h1>
@@ -9,7 +12,9 @@ const Login = () => {
       <div className="div-criar-conta">
         <p>
           ainda não tem conta <br />
-          <span className="criar-conta">Crie aqui!</span>
+          <span className="criar-conta" onClick={() => navigate("/register")}>
+            Crie aqui!
+          </span>
         </p>
       </div>
     </div>
