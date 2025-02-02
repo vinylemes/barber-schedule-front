9ae0d03ab93f40966eac9361de667e1e44107527
BrenoLemesD <brenolemesd@gmail.com>
2025-02-02 19:48:50 -0300

    feat: add gradient background to subscription page and remove old container styles

--- src/pages/subscription.tsx
+++ src/pages/subscription.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 
 const Subscription = () => {
   return (
-    <div className="container ">
+    <div className="bg-gradient-to-t from-gradient-start to-gradient-end via-gradient-end/35 w-[100vw] h-[100vh] flex flex-col items-center">
       <h1 className="titulo">Escolha um plano</h1>
       <SubscriptionCarousel />
       <Button className="bg-primary text-white my-5 w-[380px] h-[50px] b-r-10 text-lg font-bold transition-transform duration-200  hover:scale-105 hover:bg-primary">
