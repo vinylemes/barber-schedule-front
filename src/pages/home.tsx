0fc826de76ca0494ad91ea06b8ff1970344f1344
BrenoLemesD <brenolemesd@gmail.com>
2025-02-02 19:11:50 -0300

    feat: redesign home page layout and improve responsiveness

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -6,21 +6,31 @@ const Home = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="splash-container">
-      <div className="splash-image"></div>{" "}
-      {/* Added a div for the background image */}
-      <div className="button-container">
-        {" "}
-        {/* Added a container for the buttons */}
-        <Button className="button-login" onClick={() => navigate("/login")}>
-          Entrar
-        </Button>
-        <Button
-          className="button-register hover:bg-white hover:border-none"
-          onClick={() => navigate("/register")}
-        >
-          Registrar
-        </Button>
+    <div className="splash-container background-details">
+      <div className="divider"></div>
+      <div className="left-container">
+        <div className="content-container">
+          <h1 className="welcome-title" style={{ margin: "50px" }}>
+            Olá! Seja bem vindo(a)
+          </h1>
+          <div className="button-container">
+            <Button className="button-login" onClick={() => navigate("/login")}>
+              Entrar
+            </Button>
+            <Button
+              className="button-register hover:bg-white hover:border-none"
+              onClick={() => navigate("/register")}>
+              Registrar
+            </Button>
+          </div>
+        </div>
+      </div>
+      <div className="right-container">
+        <img
+          className="splash-image"
+          src="/src/assets/carinha-caindo.svg"
+          alt="Splash"
+        />
       </div>
     </div>
   );
