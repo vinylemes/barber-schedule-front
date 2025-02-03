8235a4d2ea0193f98d778920a36ef038c73895d6
GuihCFerreira <guihhh231@gmail.com>
2025-02-03 12:58:10 -0300

    feat: convert splash and add animation

--- src/pages/splash.tsx
+++ src/pages/splash.tsx
@@ -1,17 +1,44 @@
-import "../styles/splash.css";
-import splash from "../assets/splash-screen.svg";
 import { useNavigate } from "react-router-dom";
+import { Button } from "@/components/ui/button";
+import character from "../../public/character.svg";
+import { useState } from "react";
 
 const Splash = () => {
   const navigate = useNavigate();
-
-  setTimeout(() => {
-    navigate("/home");
-  }, 2000);
+  const [animationEnded, setAnimationEnded] = useState(false);
 
   return (
-    <div className="bg">
-      <img src={splash} className="img-bg" />
+    <div className="space-x-4 bg-gradient-to-t from-gradient-start to-gradient-end via-gradient-end/35 w-[100vw] h-[100vh] flex items-center pl-44">
+      <div>
+        <img
+          src={character}
+          alt=""
+          className="w-[350px] mr-20 falling"
+          onAnimationEnd={() => setAnimationEnded(true)}
+        />
+      </div>
+
+      {animationEnded && (
+        <div className="flex flex-col items-center space-y-10 fade-in">
+          <h1 className="titulo font-bold text-4xl mb-5">
+            Bem vindo ao BarberAPP!
+          </h1>
+          <div className="flex flex-col items-center">
+            <Button
+              onClick={() => navigate("/login")}
+              className="bg-primary text-white mb-3 w-[350px] h-[50px] b-r-10 text-lg transition-transform duration-200  hover:scale-105 hover:bg-primary hover:border-none"
+            >
+              Entrar
+            </Button>
+            <Button
+              onClick={() => navigate("/register")}
+              className="bg-white text-black mb-5 w-[350px] h-[50px] b-r-10 text-lg transition-transform duration-200  hover:scale-105 hover:bg-white hover:border-none"
+            >
+              Cadastro
+            </Button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
